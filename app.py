e12f5427c998232ded27554d88802a9e97acd94a
Lauren DeLago <123128869+lcarinad@users.noreply.github.com>
2023-09-23 12:49:52 -0400

    templates folder and home route

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from flask import Flask, request, render_template, redirect
-from flask_debugtoolbar import DebugToolbarExtension
+
+
 app = Flask(__name__)
-app.config['SECRET_KEY'] = "it_a_secret"
-debug = DebugToolbarExtension(app)
+
+
+@app.route('/')
+def home():
+    return render_template("index.html")
