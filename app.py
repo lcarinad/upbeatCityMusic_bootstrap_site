db559c3c7e1469c3d8ee832fc945dbb9799ca318
Lauren DeLago <123128869+lcarinad@users.noreply.github.com>
2023-09-23 13:15:03 -0400

    book purchase route, playlist route, and resources route

--- app.py
+++ app.py
@@ -7,3 +7,11 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template("index.html")
+
+@app.route('/playlist')
+def show_playlist():
+    return render_template("playlist.html")
+
+@app.route('/resources')
+def show_resources():
+        return render_template("resources.html")
